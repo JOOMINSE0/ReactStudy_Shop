3ca21689299e9b4755a44925aaec5694bb9700bb
joominse0 <sally51501512@gmail.com>
2024-05-05 21:17:13 +0900

    component만들기

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Container, Navbar, Nav} from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js'
+
+function App() {
+
+  let [shoes] = useState(data);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+
+      <div className="container">
+        <div className="row">
+          
+          {/* <Card shoes={shoes[1]} i={2}></Card>
+          <Card shoes={shoes[2]} i={3}></Card> */}
+          {
+            shoes.map((a, i)=>{
+              return(
+                <Card shoes={shoes[i]} i={i} ></Card>
+              )
+            })
+          }
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props){
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg'} width="80%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
+    </div>
+  )
+}
+
+export default App;
